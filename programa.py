93ffc93deb8155038f4ade141697625019b802b6
nicole <nicolepas@al.insper.edu.br>
2025-10-22 12:40:06 -0300

    prog

--- programa.py
+++ programa.py
@@ -1,4 +1,6 @@
-ordem = [
+from funcoes import define_posicoes,preenche_frota,faz_jogada,posiciona_frota, afundados,posicao_valida
+
+navios = [
     ("porta-aviões", 4, 1),
     ("navio-tanque", 3, 2),
     ("contratorpedeiro", 2, 3),
@@ -12,29 +14,42 @@ frota = {
     "submarino": []
 }
 
-for nome,tamanho,qnt in ordem:
-    for i in range(qnt):
-        print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
+for n, t, q in navios:
+    i = 0
+    while i < q:
+        print(f"Insira as informações referentes ao navio {n} que possui tamanho {t}")
+        linha = int(input("Linha: "))
+        col = int(input("Coluna: "))
 
-        valido = False  
-
-        while not valido:
-            linha = int(input("Linha: "))
-            coluna = int(input("Coluna: "))
-
-            if nome == "submarino":
-                orientacao = "vertical"  
+        if linha < 0 or linha > 9 or col < 0 or col > 9:
+            print("Posição fora do tabuleiro!")
+        else:
+            linha = int(linha)
+            col = int(col)
+            if linha < 0 or linha > 9 or col < 0 or col > 9:
+                print("Valores fora do tabuleiro!")
             else:
-                orientacao_num = int(input("[1] Vertical [2] Horizontal >"))
-                if orientacao_num == 1:
-                    orientacao = "vertical"
+                if t == 1:
+                    d = "horizontal"
+                    if posicao_valida(frota, linha, col, d, t):
+                        preenche_frota(frota, n, linha, col, d, t)
+                        i = i + 1
+                    else:
+                        print("Esta posição não está válida!")
                 else:
-                    orientacao = "horizontal"
-
-            if valido(frota, linha, coluna, orientacao, tamanho):
-                preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
-                valido = True  
-            else:
-                print("Esta posição não está válida!")
+                    direcao = input("[1] Vertical [2] Horizontal >").strip()
+                    if direcao == "1":
+                        d = "vertical"
+                    elif direcao == "2":
+                        d = "horizontal"
+                    else:
+                        print("Entrada inválida para orientação!")
+                        d = ""
+                    if d != "":
+                        if posicao_valida(frota, linha, col, d, t):
+                            preenche_frota(frota, n, linha, col, d, t)
+                            i = i + 1
+                        else:
+                            print("Esta posição não está válida!")
 
 print(frota)
